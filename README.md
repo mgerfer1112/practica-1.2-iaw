f6006870445f4770caf0226860523b25b7324584
mgerfer1112 <m.gervilla1112@gmail.com>
2024-10-14 12:28:49 +0000

    Instalación de Apache y MySQL

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # practica-1.2-iaw
 practica-1.2-iaw
+
+## Preparación de la máquina y sistema operativo.
+
+Para la realización de esta práctica será necesario crear una nueva instancia, en este caso con las siguientes características.
+1. SO: Red Hat.
+2. Tipo de instancia: t2.medium.
+Además deberemos tener en cuenta algunos aspectos de la configuración como unir al mismo grupo de seguridad que usamos la práctica anterior o la asignación de una IP elástica.
---
+++ Scripts/install_lamp.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+
+#Configuramos para mostrar los comandos del script y 
+#para que la ejecución se detenga cuando hay un error.
+set -ex
+
+#Actualizamos los paquetes del sistema.
+dnf update -y
+
+# !! Instalación de Apache.
+
+#Instalamos el paquete del servidor web Apache.
+sudo dnf install httpd -y
+
+#En este momento el servicio no está iniciada, no es automático.
+#Por ello hemos de iniciarlo de forma explícita.
+sudo systemctl start httpd
+
+#Habilitamos el servicio para que se inicie automáticamente 
+#dspués de cada inicio.
+sudo systemctl enable httpd
+
+# /usr/lib/systemd/system/httpd.service. se crea un url simbólico en el que
+#se alojan todos los servicios que se inician automáticamente.
+
+# !! Instalación de MySQL servr.
+
+#Instalamos MySQL Server
+sudo dnf install mysql-server -y
+
+#Iniciamos el servicio.
+sudo systemctl start mysqld
+
+#Habilitamos que se inicie después de cada reinicio.
+sudo systemctl enable mysqld
+
+systemctl restart apache
